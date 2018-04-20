8fee17e156688e248ece5f47be7e8db8db0a912c
elev <elev@172.20.0.232>
2018-04-20 15:42:45 +0200

    fix på steg 1 och 2, printar nu även ut output

--- Uppgift3_Collections/src/Main.java
+++ Uppgift3_Collections/src/Main.java
@@ -8,37 +8,40 @@ public class Main {
 
         String command = "";
 
-        int[] arr = {1, 2, 2, 3, 4, 4};
+        int[] arr = {1, 3, 4, 2, 2, 3, 4, 4};
 
         while(!command.equals("EXIT")) {
             System.out.println("--------MENU--------\n[1] Array duplicates\n[2] Set duplicates\n"
                     + "[3] Add element\n[4] Remove first element\n[5] Map");
             command = reader.readLine();
-            if(command.equals("1") || command.equals("2")) {
+            if(command.equals("1")) {
+//printar ut arrayen och skapar en ny array och loopar igenom i for loopen
+//som sedan printar ut den nya arrayen utan dubbletter
                 System.out.println("Input: " + Arrays.toString(arr));
-//kollar efter dubbletter genom att göra två for loops
-//kollar först sista elementet och sen kollar om nästa är = förra
-//isf är det dubblett
-                for (int i = 0; i < arr.length - 1; i++) {
-                    for (int j = i+1; j < arr.length; j++) {
-                        if ((arr[i] == arr[j])) {
-                            if (command.equals("1")) {
-                                System.out.println("Duplicates: " + arr[j]);
-                            }
-//använder föregående for loop och gör om arrayen till en HashSet och printar ut
-                            if (command.equals("2")) {
-                                Set<Integer> mySet = new HashSet<>(Arrays.asList(arr[j]));
-                                System.out.println("Duplicates: " + mySet);  
-                            }
-                        }
+                Arrays.sort(arr);
+                int newArr = arr[0];
+                System.out.print("Output:");
+                for (int i = 0; i < arr.length; i++) {
+                    if (newArr != arr[i]) {
+                    	System.out.print(" " + newArr);
+                        newArr = arr[i];
                     }
                 }
+                System.out.print(" " + newArr + "\n");   
+            }
+            if(command.equals("2")) {
+//printar input from arrayen, och skapar en hashset och addar arr in i hashsetten och sedan printar ut            	
+            	System.out.println("Input: " + Arrays.toString(arr));
+            	Set<Integer> mySet = new HashSet<>();
+            	for(int i : arr) {
+            		mySet.add(i); }
+            	System.out.println("Output: " + mySet);
             }
             if(command.equals("3")) {
 //skapar en ArrayList som tar in arrayen och lägger till 5 i listan och printar ut
                 List<Integer> myList = new ArrayList<>();
-                for (int i : arr) {
-                    myList.add(i); }
+                for(int i : arr) {
+                	myList.add(i); }
                 System.out.println("Input: " + myList);
                 myList.add(5);
                 System.out.println(myList + "\n5 is added to the list");
